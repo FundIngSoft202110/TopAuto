dba269aa4fe86263a0415485fd2498834e853000
Daniel Ramirez <dankramirez@outlook.com>
2021-04-26 00:25:16 -0500

    descargarPublicaciones() de RepositorioPublicacion realizado + pequeñas modificaciones en las clases de tipo Publicacion

--- capaEntidades/src/main/java/com/topauto/capaentidades/Pregunta.java
+++ capaEntidades/src/main/java/com/topauto/capaentidades/Pregunta.java
@@ -5,6 +5,9 @@ import java.util.Date;
 
 public abstract class Pregunta extends Publicacion{
 
+    public Pregunta() {
+    }
+
     public Pregunta(String id, String titulo, String descripcion, Date fecha, int numVotos, int numDenuncias, Usuario propietario, ArrayList<Comentario> comentarios) {
         super(id, titulo, descripcion, fecha, numVotos, numDenuncias, propietario, comentarios);
     }
--- capaEntidades/src/main/java/com/topauto/capaentidades/Resenia.java
+++ capaEntidades/src/main/java/com/topauto/capaentidades/Resenia.java
@@ -5,27 +5,41 @@ import java.util.Date;
 
 public class Resenia extends Publicacion {
     private int puntuacion;
+    private Vehiculo vehiculo;
 
-    public Resenia(int puntuacion, String id, String titulo, String descripcion, Date fecha, int numVotos, int numDenuncias, Usuario propietario, ArrayList<Comentario> comentarios) {
-        super(id, titulo, descripcion, fecha, numVotos, numDenuncias, propietario, comentarios);
-        this.puntuacion = puntuacion;
+    public Resenia() {
     }
     
-    public Resenia(int puntuacion, String id, String titulo, String descripcion, Date fecha, Usuario propietario) {
+    public Resenia(int puntuacion, Vehiculo vehiculo, String id, String titulo, String descripcion, Date fecha, int numVotos, int numDenuncias, Usuario propietario, ArrayList<Comentario> comentarios) {
+        super(id, titulo, descripcion, fecha, numVotos, numDenuncias, propietario, comentarios);
+        this.puntuacion = puntuacion;
+        this.vehiculo = vehiculo;
+    }
+    
+    public Resenia(int puntuacion, Vehiculo vehiculo, String id, String titulo, String descripcion, Date fecha, Usuario propietario) {
         super(id, titulo, descripcion, fecha, propietario);
         this.puntuacion = puntuacion;
+        this.vehiculo = vehiculo;
     }
 
     public int getPuntuacion() {
         return puntuacion;
     }
 
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+    
     public void setPuntuacion(int puntuacion) {
         this.puntuacion = puntuacion;
     }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
     
     public Resenia copiar(){
-        return new Resenia(this.puntuacion, this.id, this.titulo, this.descripcion, this.fecha, this.numVotos,
+        return new Resenia(this.puntuacion, this.vehiculo, this.id, this.titulo, this.descripcion, this.fecha, this.numVotos,
                 this.numDenuncias, this.propietario, this.comentarios);
     }
 }
