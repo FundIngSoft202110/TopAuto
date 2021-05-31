257f57d83967b935f7df42f36e7698c985ddebd1
richfon <richfonpan@gmail.com>
2021-05-30 19:44:37 -0500

    sin descarga

--- capaNegocio/src/test/java/com/topauto/capanegocio/ControladorPerfilTest.java
+++ capaNegocio/src/test/java/com/topauto/capanegocio/ControladorPerfilTest.java
@@ -61,7 +61,6 @@ public class ControladorPerfilTest {
         ControladorPerfil instance = new ControladorPerfil();
         boolean result = instance.esCorreo(cadena);
         assertTrue(cadena+" es correo", result);
-        fail("error");
     }
    
     /**
@@ -84,7 +83,6 @@ public class ControladorPerfilTest {
         System.out.println("validarCorreo");
         String correo = "richfonpan@yahoo.com";
         ControladorPerfil instance = new ControladorPerfil();
-        instance.descargarDatos();
         boolean result = instance.validarCorreo(correo);
         assertTrue(correo+" es valido", result);
     }
@@ -97,7 +95,6 @@ public class ControladorPerfilTest {
         System.out.println("validarUserName");
         String username = "richfon";
         ControladorPerfil instance = new ControladorPerfil();
-        instance.descargarDatos();
         boolean result = instance.validarUserName(username);
         assertTrue("funciono", result);
     }
@@ -111,7 +108,6 @@ public class ControladorPerfilTest {
         String identificador = "richfon";
         String contrasenia = "richfon123";
         ControladorPerfil instance = new ControladorPerfil();
-        instance.descargarDatos();
         boolean result = instance.acceder(identificador, contrasenia);
         assertTrue("funciono", result);
     }
@@ -124,7 +120,6 @@ public class ControladorPerfilTest {
         System.out.println("registrarPerfil");
         Usuario usuario = new Usuario("Arturo", "R2", "r2d2@gmail.com", "R2123" );
         ControladorPerfil instance = new ControladorPerfil();
-        instance.descargarDatos();
         boolean result = instance.registrarPerfil(usuario);
         assertTrue("se registro", result);      
     }
@@ -137,7 +132,6 @@ public class ControladorPerfilTest {
         System.out.println("modificarPerfil");
         Usuario usuario = null;
         ControladorPerfil instance = new ControladorPerfil();
-        instance.descargarDatos();
         boolean result = instance.modificarPerfil(usuario);
         assertTrue("Modifico", result);
     }
@@ -149,9 +143,8 @@ public class ControladorPerfilTest {
     public void testModificarContraseña() {
         System.out.println("modificarContrasenia");
         String identificador = "ramo";
-            String nueva = "ramo124";
+        String nueva = "ramo124";
         ControladorPerfil instance = new ControladorPerfil();
-        instance.descargarDatos();
         boolean result = instance.modificarContraseña(identificador, nueva);
         assertTrue("se modifico", result);
     }
